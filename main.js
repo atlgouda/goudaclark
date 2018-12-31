89539268e5cce4ea23b4b769ce0755d626378d9a
Gouda <gouda.clark@gmail.com>
2018-12-31 12:25:35 -0500

    added portfolio section

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 function myFunction() {
     var elmnt = document.getElementById("content");
     elmnt.scrollIntoView();
+};
+
+function experienceScroll() {
+    var elmnt = document.getElementById("experience");
+    elmnt.scrollIntoView();
+};
+function portfolioScroll() {
+    var elmnt = document.getElementById("portfolio");
+    elmnt.scrollIntoView();
 }

94769da1a677e3629ba6a257c1c2ed839a7f5e64
Gouda <gouda.clark@gmail.com>
2019-01-02 15:06:40 -0500

    modal operational not styled

--- main.js
+++ main.js
@@ -12,21 +12,3 @@ function portfolioScroll() {
     var elmnt = document.getElementById("portfolio");
     elmnt.scrollIntoView();
 };
-
-// Modal JS-parks
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-span.onclick = function() {
-    modal.style.display = "none";
-}
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-

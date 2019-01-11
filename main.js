411c52e51a654e3e89f8261523c836b5b4a06c92
Gouda <gouda.clark@gmail.com>
2019-01-11 13:55:04 -0500

    moved js to file

--- main.js
+++ main.js
@@ -16,3 +16,86 @@ function contactScroll() {
     var elmnt = document.getElementById("footer-div");
     elmnt.scrollIntoView();
 };
+
+        // Get the modal
+        var modal = document.getElementById('myModal');
+        // Get the button that opens the modal
+        var btn = document.getElementById("myBtn");
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+        var cancel = document.getElementById("cancel-btn");
+        cancel.onclick = function () {
+            modal.style.display = "none"
+        }
+        // When the user clicks the button, open the modal 
+        btn.onclick = function () {
+            modal.style.display = "block";
+        }
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function () {
+            modal.style.display = "none";
+
+        }
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+        // Jeopardy
+        var jModal = document.getElementById('jeopardyModal');
+        var jBtn = document.getElementById("jeopardyBtn");
+        var jSpan = document.getElementById("jeopardyClose");
+        var jcancel = document.getElementById("jeopardyCancel");
+        jcancel.onclick = function () {
+            jModal.style.display = "none"
+        }
+        jBtn.onclick = function () {
+            jModal.style.display = "block";
+        }
+        jSpan.onclick = function () {
+            jModal.style.display = "none";
+        }
+        window.onclick = function (event) {
+            if (event.target == jModal) {
+                jModal.style.display = "none";
+            }
+        }
+        // League of your imagination
+        var lModal = document.getElementById('leagueModal');
+        var lBtn = document.getElementById("leagueBtn");
+        var lSpan = document.getElementById("leagueClose");
+        var lcancel = document.getElementById("leagueCancel");
+        lcancel.onclick = function () {
+            lModal.style.display = "none"
+        }
+        lBtn.onclick = function () {
+            lModal.style.display = "block";
+        }
+        lSpan.onclick = function () {
+            lModal.style.display = "none";
+        }
+        window.onclick = function (event) {
+            if (event.target == lModal) {
+                lModal.style.display = "none";
+            }
+        }
+        // Restaurant Guide 
+        var rModal = document.getElementById('restModal');
+        var rBtn = document.getElementById("restBtn");
+        var rSpan = document.getElementById("restClose");
+        var rcancel = document.getElementById("restCancel");
+        rcancel.onclick = function () {
+            rModal.style.display = "none"
+        }
+        rBtn.onclick = function () {
+            rModal.style.display = "block";
+        }
+        rSpan.onclick = function () {
+            rModal.style.display = "none";
+        }
+        window.onclick = function (event) {
+            if (event.target == lModal) {
+                rModal.style.display = "none";
+            }
+        }
